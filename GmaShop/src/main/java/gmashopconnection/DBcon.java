91b203d354feedd5383c4809b2df245def2122fc
mariodl00 <m.deluca99@studenti.unisa.it>
2024-05-07 17:57:04 +0200

    ok

--- GmaShop/src/main/java/gmashopconnection/DBcon.java
+++ GmaShop/src/main/java/gmashopconnection/DBcon.java
@@ -9,7 +9,7 @@ public class DBcon {
 	
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		if(connection == null) {
-			Class.forName("com.sql.cj.jdbc.Driver");
+			Class.forName("com.mysql.cj.jdbc.Driver");
 		connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3308/magazzino", "root", "Mario.105");
 		System.out.print("connected");
 		}
