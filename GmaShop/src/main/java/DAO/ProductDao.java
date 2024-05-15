ec937a0fa3b88654619cc05436ae1fa7baad3fdc
gianm <gianm@Gianmarco.homenet.telecomitalia.it>
2024-05-15 16:06:35 +0200

    aggiungo productdao

---
+++ GmaShop/src/main/java/DAO/ProductDao.java
@@ -0,0 +1,41 @@
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+import java.util.*;
+import gmashopmodel.*;
+public class ProductDao {
+	    private Connection con;
+		private String query;
+		private PreparedStatement pst;
+		private ResultSet rs;
+		public ProductDao (Connection con) {
+			super();
+			this.con=con;
+			}
+		
+		public List <Product> getAllProducts() {
+			List <Product> products=new ArrayList<Product>();
+			try {
+				query="select * from products";
+				pst=this.con.prepareStatement(query);
+				rs=pst.executeQuery();
+				while(rs.next()) {
+					Product row=new Product();
+					row.setId(rs.getInt("id"));
+					row.setName(rs.getString("name"));
+					row.setCategory(rs.getString("category"));
+					row.setPrice(rs.getString("price"));
+					row.setImage(rs.getString("image"));
+					
+					products.add(row);
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			return products;
+			
+		}
+}
