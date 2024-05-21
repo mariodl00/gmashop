ee9fb54e8e720e62f19f3a0f952fc03499a1c267
aporta02 <adrianoaporta@icloud.com>
2024-05-21 19:17:39 +0200

    Correzione errore in OrderDaro.java

--- GmaShop/src/main/java/DAO/OrderDao.java
+++ GmaShop/src/main/java/DAO/OrderDao.java
@@ -2,35 +2,39 @@ package DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import gmashopmodel.Order;
+import gmashopmodel.*;
 
 public class OrderDao {
-	private Connection con;
-	private String query;
-	private PreparedStatement pst;
-	private ResultSet rs;
 	
+	private Connection con;
+
+	private String query;
+    private PreparedStatement pst;
+
+    
+    
+
 	public OrderDao(Connection con) {
+		super();
 		this.con = con;
 	}
-	
+
 	public boolean insertOrder(Order model) {
-		boolean result = false;
-		try {
-			query = "insert into orders (p_id, u_id, o_quantity, o_date) values(?,?,?,?)";
-			pst =this.con.prepareStatement(query);
-			pst.setInt(1, model.getId());
-			pst.setInt(1, model.getUid());
-			pst.setInt(1, model.getQuantity());
-			pst.setString(1, model.getDate());
-			pst.executeUpdate();
-			result= true;
-			
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
+        boolean result = false;
+        try {
+            query = "insert into orders (p_id, u_id, o_quantity, o_date) values(?,?,?,?)";
+            pst = this.con.prepareStatement(query);
+            pst.setInt(1, model.getId());
+            pst.setInt(2, model.getUid());
+            pst.setInt(3, model.getQuantity());
+            pst.setString(4, model.getDate());
+            pst.executeUpdate();
+            result = true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
 }
